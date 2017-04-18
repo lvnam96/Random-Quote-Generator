083a74644674f209db484bd1e71ff0e871c39bb0
lvnam96 <lvnam96@gmail.com>
2017-04-18 13:27:16 +0700

    get random quote from an API

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,45 +1,29 @@
-const quotes = [
-	{
-		quote:'Love is the only force capable of transforming an enemy into a friend.',
-		source:'Martin Luther King, Jr.'
-	},
-	{
-		quote:'My best friend is the one who brings out the best in me.',
-		source:'Henry Ford'
-	},
-	{
-		quote:'The greatest gift of life is friendship, and I have received it.',
-		source:'Hubert H. Humphrey'
-	},
-	{
-		quote:'Friends show their love in times of trouble, not in happiness.',
-		source:'Euripides'
-	},
-	{
-		quote:'One of the most beautiful qualities of true friendship is to understand and to be understood.',
-		source:'Lucius Annaeus Seneca'
-	},
-	{
-		quote:'Share your smile with the world. It\'s a symbol of friendship and peace.',
-		source:'Christie Brinkley'
-	},
-	{
-		quote:'The greatest healing therapy is friendship and love.',
-		source:'Hubert H. Humphrey'
-	},
-	{
-		quote:'There is nothing on this earth more to be prized than true friendship.',
-		source:'Thomas Aquinas'
-	}
-],
-random = max => {
-	return  Math.floor(Math.random() * (max - 1));
-},
-printQuote = () => {
-	let randNum = random(quotes.length);
-	document.querySelector('.quote').textContent = quotes[randNum].quote;
-	document.querySelector('.source').textContent = quotes[randNum].source;
+// var currentQuote = '', currentAuthor = '';
+//   $('#tweet-quote').on('click', function() {
+//     if(!inIframe()) {
+//       openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+//     }
+//   });
+
+function getQuote(e) {
+	$.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(response) {
+      var r = JSON.parse(response);
+      document.getElementsByClassName('quote')[0].textContent = r.quote;
+      console.log(r.quote);
+      document.getElementsByClassName('source')[0].textContent = r.author;
+      console.log(r.author);
+    }
+  });
 };
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.querySelector('#loadQuote').addEventListener("click", printQuote, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  getQuote();
+	document.getElementById('loadQuote').addEventListener('click', getQuote, false);
+});
