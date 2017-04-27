638d09d4d1b39a7155f5bf7a24a380958336c7ca
lvnam96 <lvnam96@gmail.com>
2017-04-27 16:30:49 +0700

    Redesigned interface

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,29 +1,32 @@
-// var currentQuote = '', currentAuthor = '';
-//   $('#tweet-quote').on('click', function() {
-//     if(!inIframe()) {
-//       openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-//     }
-//   });
+document.addEventListener('DOMContentLoaded', () => {
+  const loadQuoteBtn = document.getElementById('loadQuote'),
+  tweetBtn = document.getElementById('tweetBtn'),
+  getQuote = e => {
+    $.ajax({
+      headers: {
+        'X-Mashape-Key': 'hwDTMUUCVAmshcSC33BAIIq3wLunp1WhqJcjsn42Fmxrg5qaMU',
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous&count=1',
+      success: function(response) {
+        const r = JSON.parse(response);
+        document.getElementsByClassName('quote')[0].textContent = r.quote;
+        document.getElementsByClassName('source')[0].textContent = r.author;
+      }
+    });
+  };
 
-function getQuote(e) {
-	$.ajax({
-    headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: function(response) {
-      var r = JSON.parse(response);
-      document.getElementsByClassName('quote')[0].textContent = r.quote;
-      console.log(r.quote);
-      document.getElementsByClassName('source')[0].textContent = r.author;
-      console.log(r.author);
-    }
-  });
-};
-
-document.addEventListener('DOMContentLoaded', function() {
   getQuote();
-	document.getElementById('loadQuote').addEventListener('click', getQuote, false);
+
+	loadQuoteBtn.addEventListener('click', getQuote, false);
+
+	tweetBtn.addEventListener('click', () => {
+		let quote = document.getElementsByClassName('quote')[0].textContent,
+    author = document.getElementsByClassName('source')[0].textContent,
+		//there's a bug: if there iss a semicolon (;) in the quote, the part after that semicolon of the quote shared on twitter will be missed.
+		shareURL = `http:\/\/twitter.com\/share?text="${quote}" by ${author}.&related=lvnam96&hashtags=quote,super_quote&via=lvnam96`;
+		window.open(shareURL,'Share this shit on Twitter!');
+	});
 });
+
